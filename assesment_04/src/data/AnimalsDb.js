31017a8e359a921878792b1dfc6bc130093bf1e7
Maleesha Rathnayake <139684247+Maleesha2001-malee@users.noreply.github.com>
2024-11-21 10:49:33 +0000

    edit database file

--- assesment_04/src/data/AnimalsDb.js
+++ assesment_04/src/data/AnimalsDb.js
@@ -1,21 +1,20 @@
 const animals = [
-    { id: 1, name: 'Dog', image: '/workspaces/IT3133-Assesment_04/assesment_04/src/assets/1.jpg' },
-    { id: 2, name: 'Rabbit', image: '/workspaces/IT3133-Assesment_04/assesment_04/src/assets/2.jpg' },
-    { id: 3, name: 'Tiger', image: '/workspaces/IT3133-Assesment_04/assesment_04/src/assets/3.jpg' },
-    { id: 4, name: 'Giraffe', image: '/workspaces/IT3133-Assesment_04/assesment_04/src/assets/4.jpg' },
-    { id: 5, name: 'Lion', image: '/workspaces/IT3133-Assesment_04/assesment_04/src/assets/5.jpg' },
-    { id: 6, name: 'Panda', image: '/workspaces/IT3133-Assesment_04/assesment_04/src/assets/6.jpg' },
-    { id: 7, name: 'Owl', image: '/workspaces/IT3133-Assesment_04/assesment_04/src/assets/7.jpg' },
-    { id: 8, name: 'Elephant', image: '/workspaces/IT3133-Assesment_04/assesment_04/src/assets/8.jpg' },
-    { id: 9, name: 'Cat', image: '/workspaces/IT3133-Assesment_04/assesment_04/src/assets/9.jpg' },
-    { id: 10, name: 'Sheep', image: '/workspaces/IT3133-Assesment_04/assesment_04/src/assets/10.jpg' },
-    { id: 11, name: 'Donkey', image: '/workspaces/IT3133-Assesment_04/assesment_04/src/assets/11.jpg' },
-    { id: 12, name: 'Fox', image: '/workspaces/IT3133-Assesment_04/assesment_04/src/assets/12.jpg' },
-    { id: 13, name: 'Fox', image: '/workspaces/IT3133-Assesment_04/assesment_04/src/assets/13.jpg' },
-    { id: 14, name: 'Bear', image: '/workspaces/IT3133-Assesment_04/assesment_04/src/assets/14.jpg' },
-    { id: 15, name: 'Hen', image: '/workspaces/IT3133-Assesment_04/assesment_04/src/assets/15.jpg' },
-    { id: 16, name: 'goat', image: '/workspaces/IT3133-Assesment_04/assesment_04/src/assets/16.jpg' }
-  ];
+  { id: 1, name: 'Dog', image: '../assets/img/1.jpg' },
+  { id: 2, name: 'Rabbit', image: '../assets/img/2.jpg' },
+  { id: 3, name: 'Tiger', image: '../assets/img/3.jpg' },
+  { id: 4, name: 'Giraffe', image: '../assets/img/4.jpg' },
+  { id: 5, name: 'Lion', image: '../assets/img/5.jpg' },
+  { id: 6, name: 'Panda', image: '../assets/img/6.jpg' },
+  { id: 7, name: 'Owl', image: '../assets/img/7.jpg' },
+  { id: 8, name: 'Elephant', image: '../assets/img/8.jpg' },
+  { id: 9, name: 'Cat', image: '../assets/img/9.jpg' },
+  { id: 10, name: 'Sheep', image: '../assets/img/10.jpg' },
+  { id: 11, name: 'Donkey', image: '../assets/img/11.jpg' },
+  { id: 12, name: 'Fox', image: '../assets/img/12.jpg' },
+  { id: 13, name: 'Fox', image: '../assets/img/13.jpg' },
+  { id: 14, name: 'Bear', image: '../assets/img/14.jpg' },
+  { id: 15, name: 'Hen', image: '../assets/img/15.jpg' },
+  { id: 16, name: 'Goat', image: '../assets/img/16.jpg' },
+];
 
-  export default animals;
-  
+export default animals;
