000c9c55554d46a7ce2b055fabf3045796ebb867
Maleesha Rathnayake <139684247+Maleesha2001-malee@users.noreply.github.com>
2024-11-21 08:22:19 +0000

    added images

---
+++ assesment_04/src/data/AnimalsDb.js
@@ -0,0 +1,12 @@
+const animals = [
+    { id: 1, name: 'Dog', image: '1.jpg' },
+    { id: 2, name: 'Rabbit', image: '2.jpg' },
+    { id: 3, name: 'Elephant', image: 'path_to_elephant_image.jpg' },
+    { id: 4, name: 'Giraffe', image: 'path_to_giraffe_image.jpg' },
+    { id: 5, name: 'Zebra', image: 'path_to_zebra_image.jpg' },
+    { id: 6, name: 'Monkey', image: 'path_to_monkey_image.jpg' },
+    { id: 7, name: 'Bear', image: 'path_to_bear_image.jpg' },
+    { id: 8, name: 'Panda', image: 'path_to_panda_image.jpg' },
+    { id: 9, name: 'Kangaroo', image: 'path_to_kangaroo_image.jpg' },
+    { id: 10, name: 'Fox', image: 'path_to_fox_image.jpg' },
+  ];
