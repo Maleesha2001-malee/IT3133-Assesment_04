2a0b415e6face7e55753dc740411423de759763d
Maleesha Rathnayake <139684247+Maleesha2001-malee@users.noreply.github.com>
2024-11-21 08:58:04 +0000

    edit the animalCard.js file

--- assesment_04/src/components/AnimalCard.js
+++ assesment_04/src/components/AnimalCard.js
@@ -1,1 +1,10 @@
-import 
+import React from 'react';
+
+export default function Animal({animal,onAnimalClick}){
+    return(
+        <img src={animal.image} alt={animal.name}
+         onClick={()=> onAnimalClick(animal)}
+         style={{width:'100px',height:'100px',margin:'10px',cursor: 'pointer'}}
+         />
+    );
+}
